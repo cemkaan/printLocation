f53f4d5534af226b90514ef851e77894380ee9ac
CEM KAAN KÖSALI <cemkaan@inbox.ru>
2021-04-17 23:24:26 +0300

    record

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-A simple command-line application.
+Should print a json list
 
 Created from templates made available by Stagehand under a BSD-style
 [license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
--- bin/printLocation.dart
+++ bin/printLocation.dart
@@ -2,44 +2,28 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 void main() {
-  LocationsApi.getGameLocations().then((value) => print(value.last));
+  LocationsApi.getGameLocations().then((value) => value.forEach((element) {
+        print(element.id);
+      }));
 }
 
 class Location {
   final int id;
-  final String isim;
-  final String tarif;
-  final String ipucu;
-  final double enlem;
-  final double boylam;
-  final int pazaraM;
 
-  const Location(
-      {this.id,
-      this.isim,
-      this.tarif,
-      this.ipucu,
-      this.enlem,
-      this.boylam,
-      this.pazaraM});
+  const Location({this.id});
 
   static Location fromJson(json) => Location(
         id: json['id'],
-        isim: json['isim'],
-        tarif: json['tarif'],
-        ipucu: json['ipucu'],
-        enlem: json['enlem'],
-        boylam: json['boylam'],
-        pazaraM: json['pazara_m'],
       );
 }
 
 class LocationsApi {
   static Future<List> getGameLocations() async {
-    final url =
-        Uri.parse('https://api.jsonbin.io/v3/b/606b477a8be464182c592d16');
+    final url = Uri.parse('https://api.jsonbin.io/b/606b477a8be464182c592d16');
+    // Uri.parse('https://api.jsonbin.io/v3/b/606b477a8be464182c592d16');
     final response = await http.get(url);
     final body = json.decode(response.body);
+    // final body = json.decode(response.body).record;
     return body.map<Location>(Location.fromJson).toList();
   }
 }
