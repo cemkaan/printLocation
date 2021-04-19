1a9fe8af36ac34beda03da5417b568eadcee4ab7
CEM KAAN KÖSALI <cemkaan@inbox.ru>
2021-04-19 11:57:51 +0300

    all wrong

--- bin/printLocation.dart
+++ bin/printLocation.dart
@@ -19,11 +19,10 @@ class Location {
 
 class LocationsApi {
   static Future<List> getGameLocations() async {
-    final url = Uri.parse('https://api.jsonbin.io/b/606b477a8be464182c592d16');
-    // Uri.parse('https://api.jsonbin.io/v3/b/606b477a8be464182c592d16');
+    final url =
+        Uri.parse('https://api.jsonbin.io/v3/b/606b477a8be464182c592d16');
     final response = await http.get(url);
-    final body = json.decode(response.body);
-    // final body = json.decode(response.body).record;
-    return body.map<Location>(Location.fromJson).toList();
+    final body = await json.decode(response.body);
+    return body.record.map<Location>(Location.fromJson).toList();
   }
 }
